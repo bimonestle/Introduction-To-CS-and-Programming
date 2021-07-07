be149eed8d7e2412698390cb6c8869e19a9a4904
bimonestle <bimonestle@yahoo.co.id>
2021-07-07 23:39:48 +0700

    add semi-annual salary raise input

---
+++ ps1b.py
@@ -0,0 +1,27 @@
+def houseHunting():
+    totalCost = int(input('Enter the cost of your dream home: '))
+    portionDownPayment = 0.25
+    downPayment = portionDownPayment * totalCost
+    print("The downpayment of your house is %.2f" % (totalCost * portionDownPayment))
+
+    annualSalary = int(input('Enter your annual salary: '))
+    monthlySalary = annualSalary / 12
+    print('Your monthly salary is %.2f' % (monthlySalary))
+
+    semiAnnualSalaryRaise = float(input('Enter the semi-annual salary raise, as a decimal: '))
+
+    currentSavings = 0
+    investmentPerc = 0.04
+    portionSaved = float(input('Enter the percentage of your salary to save, as a decimal: '))
+    monthlySaving = monthlySalary * portionSaved
+    print('Your monthly savings will be %.2f' % (monthlySaving))
+
+    month = 0
+    while currentSavings < downPayment:
+        currentSavings += monthlySaving + (currentSavings * investmentPerc / 12)
+        month += 1
+
+    print('Number of months to get your dream home: %.2f' % (month))
+    print('The current savings are %d' % (currentSavings))
+
+houseHunting()

09b4a9a9dbf60e36e8f58770db218049c69d0549
bimonestle <bimonestle@yahoo.co.id>
2021-07-16 23:04:49 +0700

    Working on bisection. Debugging

--- ps1c.py
+++ ps1c.py
@@ -19,34 +19,61 @@ def houseHunting():
     low = 0
     high = rate
     guess = (high + low) / 2
+    # guess = rate / 2
     while currentSavings < downPayment:
+        print('Monthly Salary: %d' % (monthlySalary))
+        print('The guess rate: %d' % (guess))
         decimalRate = guess / 10000
+        print('decimal rate: %.4f' % (decimalRate))
         monthlySaving = monthlySalary * decimalRate
+        print('monthly saving: %d' % (monthlySaving))
         currentSavings += monthlySaving + (currentSavings * investmentPerc / 12)
-
+        print(monthlySaving, currentSavings, investmentPerc)
         print('Current savings: %d' % (currentSavings))
+
         print('High rate: %d' % (guess))
         print('Low rate: %d' % (guess))
-
-        if month < target and currentSavings > downPayment:
-            currentSavings = 0
-            high = guess
-            guess = (high + low) / 2
-            bisectionSteps += 1
-            # month = 0
-        elif month > target and currentSavings < downPayment:
-            currentSavings = 0
-            low = guess
-            guess = (high + low) / 2
-            bisectionSteps += 1
-            # month = 0
+        print('Month: %d' % (month))
+        print("Bisection steps: %d" % (bisectionSteps))
 
         month += 1
         if month % 6 == 0:
             monthlySalary += (monthlySalary * semiAnnualSalaryRaise)
 
+        if month == (target - 1):
+            if currentSavings > downPayment:
+                break
+            else:
+                currentSavings = 0
+                month = 0
+                high = guess
+                guess = (high + low) / 2
+                bisectionSteps += 1
+                monthlySalary = annualSalary / 12
+        elif month >= target:
+            currentSavings = 0
+            month = 0
+            low = guess
+            guess = (high + low) / 2
+            bisectionSteps += 1
+            monthlySalary = annualSalary / 12
+        
+        # if currentSavings > downPayment:
+        #     currentSavings = 0
+        #     high = guess
+        #     month = 0
+        #     guess = (high + low) / 2
+        #     bisectionSteps += 1
+        # else:
+        #     if month > (target - 1):
+        #         currentSavings = 0
+        #         low = guess
+        #         month = 0
+        #         guess = (high + low) / 2
+        #         bisectionSteps += 1
+
     print('Number of months to get your dream home: %.2f' % (month))
-    print("Bisection steps: %d" % (bisectionSteps))
     print('The current savings are %d' % (currentSavings))
+    print('The best rate is %.4f' % (guess / 10000))
 
 houseHunting()
